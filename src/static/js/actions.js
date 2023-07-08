1a6c9de036059b9b58de307a824d2a782e2c6f38
nalo_ <nath.v26@gmail.com>
2023-07-08 15:33:19 +0200

    :sparkles: Add button

--- src/static/js/actions.js
+++ src/static/js/actions.js
@@ -1,26 +1,42 @@
-createListeners(".save", "POST");
-createListeners(".delete", "DELETE");
+multipleSelectors(".save", "POST");
+multipleSelectors(".delete", "DELETE");
 
 
-function createListeners(selector, method) {
+function multipleSelectors(selector, method) {
     document.querySelectorAll(selector).forEach((button) => {
         var childs = button.parentElement.parentElement.children;
         var source = childs[0].firstChild.innerHTML;
         var target = childs[2].firstChild.innerHTML;
         button.addEventListener("click", (e) => {
-            fetch(source, {
-                method: method,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    target: target,
-                })
-            }).then(() => {
-                window.location.href = '/';
-            }).catch((error) => {
-                console.log(error);
-            });
+            send_data(method, source, target);
         });
     });
 }
+function send_data(method, source, target) {
+    fetch(source, {
+        method: method,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            target: target,
+        })
+    }).then(() => {
+        window.location.href = '/';
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+document.querySelector(".add").addEventListener("click", () => {
+    var source = document.querySelector("#source").value;
+    var target = document.querySelector("#target").value;
+    if (source == "" || target == "") {
+        alert("Please fill in all fields");
+        return;
+    }
+    if (source.charAt(0) == "/")
+        source = source.substring(1);
+
+    send_data("PUT", source, target);
+});
