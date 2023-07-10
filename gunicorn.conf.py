73ca331487e0c9b30e7d78810d60f9ee246e12c1
nalo_ <nath.v26@gmail.com>
2023-07-10 10:53:38 +0200

    :rocket: Gunicorn logs

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 bind = os.getenv("WEB_HOST") + ":" + str(os.getenv("WEB_PORT"))
+accesslog = "-"
+access_log_format = '%(t)s %({x-forwarded-for}i)s %(l)s "%(r)s" %(s)s'  # "%(a)s"'
